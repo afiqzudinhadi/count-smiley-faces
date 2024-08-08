26f9ea170acc6ca92142718c09b3c9da40a765b7
afiqzudinhadi <afiqzudinhadi@gmail.com>
2024-08-08 14:04:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function countSmileys(arr) {
 	const smileyPattern = /^[:;][-~]?[)D]$/;
+
+	if (arr.length === 0) {
+		return 0;
+	}
+
 	return arr.filter((face) => smileyPattern.test(face)).length;
 }
 
@@ -7,3 +12,4 @@ function countSmileys(arr) {
 console.log(countSmileys([":)", ";(", ";}", ":-D"])); // Output: 2
 console.log(countSmileys([";D", ":-(", ":-)", ";~)"])); // Output: 3
 console.log(countSmileys([";]", ":[", ";*", ":$", ";-D"])); // Output: 1
+console.log(countSmileys([])); // Output: 0
